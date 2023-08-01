abcec864c39d053ee3187e086b1a307c028832a6
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-08-01 11:15:33 +0700

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-//const PORT = process.env.PORT || 3001
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+//const PORT = 3001
 const MONGODB_URI = process.env.MONGODB_URI
 
 //const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
